cb8208fb2570186932202aeb00b372f90232b9f0
Stefan Nilsson <stefan.nilsson@ist.com>
2024-10-07 13:44:52 +0200

    Added suport for enums

--- oas2xsd.py
+++ oas2xsd.py
@@ -14,7 +14,7 @@ def yaml_type_to_xsd_type(yaml_type):
     }
     return mapping.get(yaml_type, "xs:string")
 
-def create_xsd_element(element_name, element_type=None, required=False, is_array=False, complex_type=None):
+def create_xsd_element(element_name, element_type=None, required=False, is_array=False, complex_type=None, enum_values=None):
     """Create an XML Schema element."""
     elem = ET.Element('xs:element', name=element_name)
     
@@ -25,9 +25,15 @@ def create_xsd_element(element_name, element_type=None, required=False, is_array
         elem.set("minOccurs", "1")
     else:
         elem.set("minOccurs", "0")
-    
-    # Use the complexType if provided, otherwise set the type
-    if complex_type is not None:
+
+    if enum_values:
+        restriction = ET.Element('xs:restriction', base=element_type)
+        for value in enum_values:
+            ET.SubElement(restriction, 'xs:enumeration', value=value)
+        simple_type = ET.Element('xs:simpleType')
+        simple_type.append(restriction)
+        elem.append(simple_type)
+    elif complex_type is not None:
         elem.append(complex_type)
     else:
         elem.set('type', element_type)
@@ -42,24 +48,66 @@ def resolve_ref(ref_path, openapi_spec):
         ref_schema = ref_schema.get(part, {})
     return ref_schema
 
+def merge_all_of_schemas(all_of_list, openapi_spec):
+    """Merge multiple schemas defined in an allOf list into a single schema."""
+    merged_properties = {}
+    required_fields = []
+    for schema in all_of_list:
+        if '$ref' in schema:
+            ref_schema = resolve_ref(schema['$ref'], openapi_spec)
+            properties = ref_schema.get('properties', {})
+            required = ref_schema.get('required', [])
+        else:
+            properties = schema.get('properties', {})
+            required = schema.get('required', [])
+        merged_properties.update(properties)
+        required_fields.extend(required)
+    return merged_properties, list(set(required_fields))  # Remove duplicates from required fields
+
 def process_properties(properties, required_fields, openapi_spec):
     """Create XSD complexType elements based on OpenAPI properties, handling multiple levels of nested objects."""
     complex_type = ET.Element('xs:complexType')
     sequence = ET.SubElement(complex_type, 'xs:sequence')
     
     for prop_name, prop_details in properties.items():
+        # Handle allOf construct
+        if 'allOf' in prop_details:
+            merged_properties, merged_required = merge_all_of_schemas(prop_details['allOf'], openapi_spec)
+            nested_complex_type = process_properties(merged_properties, merged_required, openapi_spec)
+            sequence.append(create_xsd_element(prop_name, complex_type=nested_complex_type, required=True))
+
+        # Handle anyOf construct by creating a choice element
+        elif 'anyOf' in prop_details:
+            choice_type = ET.Element('xs:choice')
+            for option in prop_details['anyOf']:
+                if '$ref' in option:
+                    ref_schema = resolve_ref(option['$ref'], openapi_spec)
+                    nested_complex_type = process_properties(ref_schema.get('properties', {}), ref_schema.get('required', []), openapi_spec)
+                    choice_type.append(ET.Element('xs:element', type="xs:anyType"))
+                else:
+                    yaml_type = option.get('type', 'string')
+                    xsd_type = yaml_type_to_xsd_type(yaml_type)
+                    choice_type.append(ET.Element('xs:element', type=xsd_type))
+            sequence.append(create_xsd_element(prop_name, complex_type=choice_type, required=True))
+
         # Handle the case where the property is a reference
-        if '$ref' in prop_details:
+        elif '$ref' in prop_details:
             ref_schema = resolve_ref(prop_details['$ref'], openapi_spec)
             nested_complex_type = process_properties(ref_schema.get('properties', {}), ref_schema.get('required', []), openapi_spec)
             sequence.append(create_xsd_element(prop_name, complex_type=nested_complex_type, required=True))
+
         else:
             yaml_type = prop_details.get('type', 'string')
             is_required = prop_name in required_fields
             is_array = yaml_type == "array"
 
+            # Handle enum values for strings
+            if yaml_type == 'string' and 'enum' in prop_details:
+                enum_values = prop_details['enum']
+                sequence.append(create_xsd_element(prop_name, element_type='xs:string', required=is_required, enum_values=enum_values))
+
             # Handle array types with nested items, including objects
-            if is_array:
+            elif is_array:
                 items = prop_details.get('items', {})
                 if '$ref' in items:
                     ref_schema = resolve_ref(items['$ref'], openapi_spec)
